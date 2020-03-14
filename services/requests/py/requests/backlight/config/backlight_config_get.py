260112999354d0128661733bdffa827367c42104
Christian <rosenberg-c>
2020-03-14 13:40:28 +0100

    [refactor] Change default return config

--- services/requests/py/requests/backlight/config/backlight_config_get.py
+++ services/requests/py/requests/backlight/config/backlight_config_get.py
@@ -1,8 +1,7 @@
 import os
 
 from flask import Flask, jsonify, request
-
-from requests.backlight.config.get_request_config import convert_dict_to_class, convert_json_path
+from requests.backlight.config.get_request_config import convert_dict_to_class, convert_json_path, Config
 from requests.support import BACKLIGHT_ENDPOINT
 
 
@@ -19,5 +18,14 @@ def endpoint_backlight_config_get(app: Flask, config_dir):
             if os.path.isfile(_path):
                 return validate_read(_path)
 
-            return validate_read(config_dir + "/backlight/backlight_config_default.json")
+            # return validate_read(config_dir + "/backlight/backlight_config_default.json")
+            return jsonify(Config(
+                auto_on=False,
+                auto_off=False,
+                disable_from="00:00:00",
+                enable_from="00:00:00",
+                update_cycle=6,
+                ignore_gpio=False,
+                name="Default"
+            ).__dict__)
         return jsonify(None)
--- services/requests/py/requests/energenie/config/energenie_config_get.py
+++ services/requests/py/requests/energenie/config/energenie_config_get.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask, jsonify, request
-
 from requests.energenie.config.get_request_config import convert_dict_to_class, convert_json_path, Config
 from requests.support import ENERGENIE_ENDPOINT
 
@@ -21,7 +20,12 @@ def endpoint_energenie_config_get(app: Flask, config_dir):
 
             # return validate_read(config_dir + "/energenie_config_default.json")
             return jsonify(Config(
-                auto_on=True, auto_off=True, disable_from="00:00:00", enable_from="00:00:00", update_cycle=6,
-                ignore_gpio=True, name="Default"
+                auto_on=False,
+                auto_off=False,
+                disable_from="00:00:00",
+                enable_from="00:00:00",
+                update_cycle=6,
+                ignore_gpio=False,
+                name="Default"
             ).__dict__)
         return jsonify(None)
